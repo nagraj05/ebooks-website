49e4b4c12f8472388768868c8e61e28cec1b574d
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-06-23 09:43:18 +0530

    changes in media queries part 2

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ export default function Navbar() {
   return (
     <div className="hero">
       <Navigation />
+      <div className="homepage--texts">
       <h1 className="nav--text">A WORLD IN PAGES</h1>
       <p className="nav--para">
         Embark on a literary journey where every click reveals a new chapter,
@@ -16,6 +17,7 @@ export default function Navbar() {
         <Lottie animationData={books} />
       </div>
       <p className="nav--subpara">Coded with hate, love & coffee by Nagraj</p>
+      </div>
     </div>
   );
 }
