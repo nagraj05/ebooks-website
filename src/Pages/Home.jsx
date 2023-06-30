a774a2e0d2f89915646763432ddb511e5275ce97
Nagraj <tadingi.nagraj.nagraj7@gmail.com>
2023-06-30 09:46:27 +0530

    added word cloud and challenges

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,11 +24,20 @@ function Home() {
   });
 
   return (
-    <>
+    <div className="homepage--container">
       <Navbar />
       <div className="booksbox">{bookElements}</div>
+      <div className="challenge">
+        <h3 className="challenge--heading">Reading Challenges & Lists</h3>
+        <ul className="challenge--list">
+          <li><a href="https://app.thestorygraph.com/reading_challenges/75592277-0f6e-438e-ba65-0ce6c43088d8" target="_blank" rel="noopener noreferrer">BBC Top 100 Books (C)</a></li>
+          <li><a href="">Must Read Thrillers (L)</a></li>
+          <li><a href="">Must Read Classics (L)</a></li>
+        </ul>
+      </div>
+      <div className="wordscloud"></div>
       <Footer />
-    </>
+    </div>
   );
 }
 
